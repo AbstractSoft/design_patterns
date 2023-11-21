315ba093cbd023e32593f5a46e27637d5c6ca9a7
Abstractsoft <ghergu@yahoo.com>
2023-11-21 20:59:43 +0200

    Small refactoring

--- Facade/Facade/MultimediaFacade.cs
+++ Facade/Facade/MultimediaFacade.cs
@@ -7,15 +7,15 @@ internal class MultimediaFacade
     private readonly VideoPlayer _videoPlayer = new();
     private readonly AudioPlayer _audioPlayer = new();
     private readonly Display _display = new();
-    private readonly SubtitlePlayer _subtitlePlayer = new();
+    private readonly SubtitleService _subtitleService = new();
     private readonly StreamingService _streamingService = new();
 
-    public void WatchMovie(string movie, string audioTrack, string subtitles)
+    public void WatchMovie(string movie, string audioTrack, string subtitle)
     {
         _streamingService.StreamContent(movie);
         _videoPlayer.PlayVideo(movie);
         _audioPlayer.PlayAudio(audioTrack);
-        _subtitlePlayer.DisplaySubtitles(subtitles);
+        _subtitleService.DisplaySubtitle(subtitle);
         _display.Show("Enjoy the movie on the big screen!");
     }
 
---
+++ Facade/Subsystem/SubtitleService.cs
@@ -0,0 +1,11 @@
+namespace Multimedia.Subsystem;
+
+using System;
+
+internal class SubtitleService
+{
+    public void DisplaySubtitle(string subtitle)
+    {
+        Console.WriteLine($"Displaying subtitle: {subtitle}");
+    }
+}
